4a95c41d305603eeee9d78a1a2c52f693a618899
Jeff Githiri <geoffrey.githiri@andela.com>
2019-12-04 14:21:08 +0300

    react class components

--- src/App.js
+++ src/App.js
@@ -26,8 +26,37 @@ import './App.css';
 // card
 // list
 
-const App = ({title}) => (
+class Card extends React.Component {
+    render() {
+        return (
+            <div className="github-profile">
+                <img src="https://placehold.it/75" alt=""/>
+                <div className="info">
+                    <div className="name">Name here...</div>
+                    <div className="company">Company here...</div>
+                </div>
+            </div>
+        );
+    }
+}
+
+class App extends React.Component {
+    //constructor
+    // this
+
+    render() {
+        return (
+            <div>
+                <div className="header">{this.props.title}</div>
+
+                <Card/>
+            </div>
+        )
+    }
+}
+
+/*const App = ({title}) => (
     <div className="header">{title}</div>
-);
+);*/
 
 export default App;
